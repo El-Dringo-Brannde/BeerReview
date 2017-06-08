60b91c986b89faa90fb1f25cbf233876af8a01be
El-Dringo-Brannde <Brandon.d.junk@gmail.com>
2017-06-08 16:22:59 -0700

    Cookies are set

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ mongoose.connect(url, function() {
 
 const Hapi = require('hapi');
 const server = new Hapi.Server();
+
+
 server.connection({
     port: 3000,
     host: '10.0.0.10'
 });
+server.state('session', {
+    ttl: 24 * 60 * 60 * 1000,     // One day
+    isSecure: false,
+	isHttpOnly: false,
+    path: '/'
+//    encoding: 'base64json'
+});
 
 require('./ServerSide/routes.js')(server); //require and call routes file
+
+
 server.start((err) => {
     if (err)
         throw err;
